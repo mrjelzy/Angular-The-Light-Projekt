0da598a0e7f66c5f6d2918180613794d3a432507
Ryan Bengoufa <ryan.bengoufa@etu.umontpellier.fr>
2023-09-25 22:15:51 +0200

    add google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
+declare const gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'The Light Projekt';
+
+  constructor(public router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-PWSHMJZQHL', { 'page_path': event.urlAfterRedirects });
+      }      
+    })
+  }
+
 }
